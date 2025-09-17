c1fbbd8fde6761fb0a7ecda36886ec5f018e6c8a
AyushDubey23 <ayushdubey3740@gmail.com>
2025-09-18 03:45:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Hriday Bajaj',
+  description: 'Hriday Bajaj Portfolio',
+  generator: 'ayush dubey lol',
 }
 
 export default function RootLayout({
